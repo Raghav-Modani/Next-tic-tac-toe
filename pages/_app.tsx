e4898a23c9fa0aba121ba55183bd2d18c6db24fa
Raghav-Modani <raghavmodani4@gmail.com>
2021-07-23 11:47:32 +0530

    created using nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import 'antd/dist/antd.css';
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
+import tictactoeindex from './Tic-Tac-Toe/index'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+  <Component {...pageProps} />
+  </RecoilRoot>
+  )
 }
 export default MyApp
